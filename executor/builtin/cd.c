b1d7999c11f95671d6a0079bec95de17d6127936
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2026-03-16 18:24:55 +0000

    changed ARG_MAX to PATH_MAX

--- executor/builtin/cd.c
+++ executor/builtin/cd.c
@@ -12,20 +12,22 @@
 
 #include "../../incs/minishell.h"
 #include <linux/limits.h>
+#include <limits.h>
 
 // t_token needs to be the list and not tdlist->token
-int	exec_cd(t_shell *sh, t_dlist *cmd)
+int	exec_cd(t_shell *sh)
 {
 	t_token	*token;
-	char	oldpwd[ARG_MAX];
-	char	path[ARG_MAX];
+	char	oldpwd[PATH_MAX];
+	char	path[PATH_MAX];
 
 	token = cmd->next->data;//this is confusing
 	path = token->content;
-	getcwd(oldpwd, ARG_MAX);
+	getcwd(oldpwd, PATH_MAX);
 	if (chdir(path) == -1)
 		return(/*cd error*/);
 	//envp_setvar(update OLDPWD to whats in oldpwd)
 	//envp_setvar(update PWD to whats in getcwd)
 	return ();
 }
+
