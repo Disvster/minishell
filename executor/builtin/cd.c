6ca76d833143b952951db2e20f192a3793932120
Disvster <mmcouto2001@gmail.com>
2026-05-29 11:31:24 +0100

    cd WIP

--- executor/builtin/cd.c
+++ executor/builtin/cd.c
@@ -19,3 +19,21 @@ int	exec_cd(t_shell *sh, t_cmd *cmd)
 	(void)cmd;
 	return (-1);
 }
+
+// int	exec_cd(t_shell *sh, t_cmd *cmd)
+// {
+// 	char	*oldpwd;
+//
+// 	if (cd->args && cd->args[1] != NULL)
+// 		return (cd_too_many_args());
+// 	if (!cmd->args || cmd->args[0][0] == '~' || !ft_strcmp("--", cmd->args[0]))
+// 		return (change_dir_to_key(sh, "HOME"));
+// 	else if (cmd->args[0][0] == '-')
+// 		return (change_dir_to_key(sh, "OLDPWD"));
+// 	oldpwd = getcwd(NULL, 0);
+// 	export_replace_content(search_for_key(sh, "OLDPWD"), oldpwd);
+// 	free(oldpwd);
+// 	if (chdir(cmd->args[0]) == -1)
+// 		return (cd_error(NULL, path), 1);
+// 	return (0);
+// }
